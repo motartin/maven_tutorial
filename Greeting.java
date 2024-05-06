1f4f3e9aa6c29d37c3f283cf8bc74c4d35896d50
Martin <motartin@posteo.de>
2024-05-06 17:10:00 +0200

    maven tutorial step 2

--- Greeting.java
+++
@@ -1,7 +0,0 @@
-public class Greeting {
-	private final String messgage = "Hello Maven-less simple world!";
-
-	public String getMessgage() {
-		return messgage;
-	}
-}
---
+++ your/company/Greeting.java
@@ -0,0 +1,7 @@
+public class Greeting {
+	private final String messgage = "Hello slightly less simple Maven-less world!";
+
+	public String getMessgage() {
+		return messgage;
+	}
+}
